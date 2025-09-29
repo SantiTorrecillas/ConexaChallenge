f67b33b9f8c4e4f74ecb1f2b03970207a2fb1c83
SantiTorrecillas <santiagotorrecillas99@gmail.com>
2025-09-29 19:51:07 +0200

    adding Jwt token generator

--- ConexaChallenge/Controllers/AuthController.cs
+++ ConexaChallenge/Controllers/AuthController.cs
@@ -2,12 +2,16 @@
 using ConexaChallenge.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ConexaChallenge.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IConfiguration configuration) : ControllerBase
     {
         public static User user = new();
 
@@ -22,5 +26,43 @@ namespace ConexaChallenge.Controllers
 
             return Ok(user);
         }
+
+        [HttpPost("Login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if(user.UserName != request.UserName)
+            {
+                return BadRequest("User Not Found.");
+            }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong Password.");
+            }
+
+            string token = CreateToken(user);
+            
+            return Ok(token);
+        }
+
+        private string CreateToken (User user)
+        {
+            List<Claim> claims = [ new(ClaimTypes.Name, user.UserName) ];
+
+            SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            SigningCredentials creds = new(key, SecurityAlgorithms.HmacSha512);
+
+            JwtSecurityToken tokenDescriptor = new JwtSecurityToken(
+                    issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                    audience: configuration.GetValue<string>("AppSettings:Audience"),
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddHours(1),
+                    signingCredentials: creds
+                    );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+
     }
 }
