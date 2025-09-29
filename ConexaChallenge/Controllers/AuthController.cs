ee5527c2e4b95dbf0270007fedf4172d02c05025
Home PC <Home PC@DESKTOP-ONLH5SS>
2025-09-29 19:11:33 +0200

    Add project files.

---
+++ ConexaChallenge/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using ConexaChallenge.Dtos;
+using ConexaChallenge.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConexaChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost("Register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.UserName = request.UserName;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+    }
+}
