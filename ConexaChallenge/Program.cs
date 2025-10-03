58362c938d7c3d118e367484afbaa88624bc0224
SantiTorrecillas <santiagotorrecillas99@gmail.com>
2025-10-03 09:42:11 -0300

    delete program comments

--- ConexaChallenge/Program.cs
+++ ConexaChallenge/Program.cs
@@ -8,10 +8,8 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDataBase")));
@@ -43,7 +41,6 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
